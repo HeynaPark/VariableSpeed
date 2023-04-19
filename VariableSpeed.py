4b5b0af6511d6f4185af75cc8d85dd3e3a9ee150
hnpark <hnpark@4dreplay.com>
2023-04-19 14:10:18 +0900

    code 정리, 수식 일반화

--- VariableSpeed.py
+++ VariableSpeed.py
@@ -15,11 +15,11 @@ class Circle:
 
 def calcPos(t, start, v0, v1, a1, a2):
     pos = 0
-
+    constant = 250*(v1-v0)
     if t < 500:
         pos = start + v0*t + 0.5*a1*t**2
     else:
-        pos = start + v1*t + 0.5*a2*(t-500)**2 - 500
+        pos = start + v1*t + 0.5*a2*(t-500)**2 - constant
 
     return pos
 
@@ -27,40 +27,54 @@ def calcPos(t, start, v0, v1, a1, a2):
 radius = 20
 color_c1 = (0, 255, 0)
 color_c2 = (0, 255, 255)
+color_c3 = (100, 0, 255)
+color_c4 = (255, 0, 255)
 start_x = 200
 end_x = 1200
 move_c1 = start_x
 move_c2 = start_x
+move_c3 = start_x
+move_c4 = start_x
 
 h_c1 = 200
 h_c2 = 300
+h_c3 = 400
+h_c4 = 500
 
 vel_c1 = 1/(end_x - start_x)
 
 
 cnt = 0
 while True:
-    img = np.zeros((500, 1400, 3), np.uint8)
+    img = np.zeros((600, 1400, 3), np.uint8)
 
     circle1 = Circle(move_c1, h_c1, radius, color_c1)
     circle2 = Circle(move_c2, h_c2, radius, color_c2)
+    circle3 = Circle(move_c3, h_c3, radius, color_c3)
+    circle4 = Circle(move_c4, h_c4, radius, color_c4)
 
     circle1.draw(img)
     circle2.draw(img)
+    circle3.draw(img)
+    circle4.draw(img)
     cv2.imshow('Circle', img)
     cv2.waitKey(1)
 
     alpha = cnt * vel_c1
 
     move_c1 = int(start_x + (end_x - start_x)*alpha)        # 등속운동
-    move_c2 = int(calcPos(cnt, start_x, 0, 2, 0.004, -0.004))  # 가속운동
+    move_c2 = int(calcPos(cnt, start_x, 0.5, 1.5, 0.002, -0.002))  # 가속운동
+    move_c3 = int(calcPos(cnt, start_x, 0, 2, 0.004, -0.004))  # 가속운동
+    move_c4 = int(calcPos(cnt, start_x, 0.8, 1.5, 0.0014, -0.0026))  # 가속운동
 
-    print(cnt, move_c2)
+    print(cnt, move_c3)
 
     if move_c1 > end_x:
         print(cnt)
         move_c1 = start_x
         move_c2 = start_x
+        move_c3 = start_x
+        move_c4 = start_x
         cnt = 0
     cnt += 1
 
