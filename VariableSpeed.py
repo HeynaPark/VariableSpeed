506108eebf69bc63714e9647f3018732bbe40385
hnpark <hnpark@4dreplay.com>
2023-04-27 15:59:40 +0900

    새로운 테스트 케이스 추가, calcPos 함수의 pos계산 일반화 정리

--- VariableSpeed.py
+++ VariableSpeed.py
@@ -16,9 +16,10 @@ class Circle:
 
 radius = 20
 color_c1 = (0, 255, 0)
-color_c2 = (0, 255, 255)
-color_c3 = (100, 0, 255)
-color_c4 = (255, 0, 255)
+color_c2 = (120, 255, 0)
+color_c3 = (0, 255, 255)
+color_c4 = (100, 0, 255)
+color_c5 = (255, 0, 255)
 start = 200
 end_x = 1200
 
@@ -26,11 +27,13 @@ move_c1 = start
 move_c2 = start
 move_c3 = start
 move_c4 = start
+move_c5 = start
 
 h_c1 = 200
 h_c2 = 300
 h_c3 = 400
 h_c4 = 500
+h_c5 = 600
 
 vel_c1 = 1/(end_x - start)
 
@@ -39,28 +42,24 @@ y1 = []
 y2 = []
 y3 = []
 y4 = []
+y5 = []
 
 cnt = 0
 whole_time = 1000
 duration = 1000
 
 
-def calcPos(t, type):
+def calcPos(t, type, param):
     pos = 0
     v0 = 0
     v1 = 0
     a1 = 0
     a2 = 0
 
-    if type == 'normal':
-        v0 = 0.5
+    if type == 'triangle':
+        v0 = duration/whole_time * (1-param)
         v1 = 2*duration/whole_time - v0
         a1 = 4*duration/(whole_time**2) - (4/whole_time)*v0
-        a2 = -a1
-    elif type == 'faster':
-        v0 = 0
-        v1 = 2*duration/whole_time
-        a1 = 4*duration/(whole_time**2)
         a2 = -a1
 
     elif type == 'trapezoid':
@@ -93,48 +92,106 @@ def calcPos(t, type):
     return pos
 
 
+def calcZoom(t, start_zoom, end_zoom):
+    zoom = 0
+    interval = end_zoom - start_zoom
+    v0 = interval/whole_time * 0
+    v1 = 2*interval/whole_time - v0
+    a1 = 4*interval/(whole_time**2) - (4/whole_time)*v0
+    a2 = -a1
+
+    half = whole_time * 0.5
+    constant = 0.25*whole_time*(v1-v0)
+
+    if t < half:
+        zoom = radius + v0*t + 0.5*a1*t**2
+    else:
+        zoom = radius + v1*t + 0.5*a2*(t-half)**2 - constant
+
+    return zoom
+
+
 # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 # out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (1400, 600))
 
+
+# velocity test
+# while True:
+#     img = np.zeros((700, 1400, 3), np.uint8)
+
+#     circle1 = Circle(int(move_c1), h_c1, radius, color_c1)
+#     circle2 = Circle(int(move_c2), h_c2, radius, color_c2)
+#     circle3 = Circle(int(move_c3), h_c3, radius, color_c3)
+#     circle4 = Circle(int(move_c4), h_c4, radius, color_c4)
+#     circle5 = Circle(int(move_c5), h_c5, radius, color_c5)
+
+#     circle1.draw(img)
+#     circle2.draw(img)
+#     circle3.draw(img)
+#     circle4.draw(img)
+#     circle5.draw(img)
+#     cv2.imshow('Circle', img)
+#     # out.write(img)
+#     cv2.waitKey(1)
+
+#     alpha = cnt * vel_c1
+
+#     move_c1 = int(start + (end_x - start)*alpha)        # 등속운동
+#     move_c2 = calcPos(cnt, 'triangle', 0.4)               # 가속운동
+#     move_c3 = calcPos(cnt, 'triangle', 0.8)               # 가속운동
+#     move_c4 = calcPos(cnt, 'triangle', 1)
+#     move_c5 = calcPos(cnt, 'trapezoid', 0)
+
+#     # print(cnt, move_c4)
+
+#     x.append(cnt)
+#     y1.append(move_c1)
+#     y2.append(move_c2)
+#     y3.append(move_c3)
+#     y4.append(move_c4)
+#     y5.append(move_c5)
+
+#     if cnt > whole_time:
+#         # break
+#         print(cnt)
+#         cnt = 0
+#     cnt += 1
+
+
+# out.release()
+# cv2.destroyAllWindows()
+# zoom test
 while True:
-    img = np.zeros((600, 1400, 3), np.uint8)
+    img = np.zeros((1200, 1400, 3), np.uint8)
+    r = int(radius*(1 + 4*(cnt / whole_time)))
+    r3 = int(calcZoom(cnt, radius, radius*5))
+    print(r, r3)
 
-    circle1 = Circle(int(move_c1), h_c1, radius, color_c1)
-    circle2 = Circle(int(move_c2), h_c2, radius, color_c2)
-    circle3 = Circle(int(move_c3), h_c3, radius, color_c3)
-    circle4 = Circle(int(move_c4), h_c4, radius, color_c4)
-
+    circle1 = Circle(int(move_c1), 300, r, color_c1)
     circle1.draw(img)
+    circle2 = Circle(int(move_c2), 600, r, color_c2)
     circle2.draw(img)
+    circle3 = Circle(int(move_c2), 900, r3, color_c3)
     circle3.draw(img)
-    circle4.draw(img)
+
     cv2.imshow('Circle', img)
-    # out.write(img)
     cv2.waitKey(1)
 
     alpha = cnt * vel_c1
 
-    move_c1 = int(start + (end_x - start)*alpha)        # 등속운동
-    move_c2 = calcPos(cnt, 'normal')               # 가속운동
-    move_c3 = calcPos(cnt, 'faster')
-    move_c4 = calcPos(cnt, 'trapezoid')
-
-    # print(cnt, move_c4)
-
-    x.append(cnt)
-    y1.append(move_c1)
-    y2.append(move_c2)
-    y3.append(move_c3)
-    y4.append(move_c4)
+    move_c1 = int(start + (end_x - start)*alpha)
+    move_c2 = calcPos(cnt, 'triangle', 0.5)
+    move_c3 = int(start + (end_x - start)*alpha)
+    # x.append(cnt)
+    # y1.append(move_c1)
 
     if cnt > whole_time:
         # break
         print(cnt)
         cnt = 0
+        r = radius
+        r3 = radius
     cnt += 1
-
-# out.release()
-# cv2.destroyAllWindows()
 
 
 # draw Graphs
